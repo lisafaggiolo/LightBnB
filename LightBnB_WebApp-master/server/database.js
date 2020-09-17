708add406ccd4199d85345fbf677e896b5827f2b
lisafaggiolo <lisa.faggiolo@live.ca>
2020-09-17 21:22:37 +0000

    Refactored addProperty function with pool and queries.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -127,8 +127,6 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-    console.log("options => ", options);
-    console.log("limit =>", limit);
     // 1
     const queryParams = [];
     // 2
@@ -190,12 +188,8 @@ const getAllProperties = (options, limit = 10) => {
     `;
   
     // 5
-    console.log(queryString, queryParams);
-  
-    // 6
     return pool.query(queryString, queryParams)
     .then(res => {
-      console.log(res.rows);
       return res.rows
       });
 }
@@ -208,9 +202,24 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const query = `INSERT INTO properties
+    (title, description, owner_id, cover_photo_url, thumbnail_photo_url, 
+    cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, 
+    active, province, city, country, street, post_code)
+  
+  VALUES 
+    ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
+  RETURNING *;`
+  const values = [property.title, property.description, property.owner_id, property.cover_photo_url, property.thumbnail_photo_url, 
+    property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, 
+    true, property.province, property.city, property.country, property.street, property.post_code]
+
+  return pool
+    .query(query, values)
+    .then(res => {
+      console.log(res.rows[0])
+      res.rows[0]
+    });
 }
 exports.addProperty = addProperty;
